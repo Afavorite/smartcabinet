cfd4a5d5e1b2e8e50e61befe843267a79727c17c
AFavo <lai_kaiwen@qq.com>
2022-02-11 16:10:39 +0800

    添加注册登录界面

---
+++ app/src/main/java/com/app2018212763/smartcabinet/registerActivity.java
@@ -0,0 +1,17 @@
+package com.app2018212763.smartcabinet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import java.sql.Connection;
+
+public class registerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
