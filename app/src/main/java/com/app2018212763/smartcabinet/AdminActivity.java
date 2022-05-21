4801e5992accddb6c615a9f757e746cb092b2545
Afavorite <lai_kaiwen@qq.com>
2022-05-21 11:36:10 +0800

    管理员控制开始和停止

--- app/src/main/java/com/app2018212763/smartcabinet/AdminActivity.java
+++ app/src/main/java/com/app2018212763/smartcabinet/AdminActivity.java
@@ -20,6 +20,7 @@ import android.widget.Toast;
 import com.app2018212763.smartcabinet.Bean.Order;
 import com.app2018212763.smartcabinet.Order.BoxSelectActivity;
 import com.app2018212763.smartcabinet.Order.HttpOrder;
+import com.app2018212763.smartcabinet.Order.OrderShowActivity;
 
 import java.util.Objects;
 
@@ -29,9 +30,11 @@ public class AdminActivity extends AppCompatActivity {
     private Button btn_admin_control;
     private Button btn_admin_stopcontrol;
     private Button btn_admin_opendoor;
-    private int RequestCode = 1;
+    private String control_order_number = null;
+    private Boolean isControlling = false;
 
     private final static int ADMINCONTROL_JUDGE = 1;
+    private final static int ADMINSTOPCONTROL_JUDGE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +49,10 @@ public class AdminActivity extends AppCompatActivity {
         btn_admin_control = (Button) findViewById(R.id.btn_admin_control);
         btn_admin_stopcontrol = (Button) findViewById(R.id.btn_admin_stopcontrol);
         btn_admin_opendoor = (Button) findViewById(R.id.btn_admin_opendoor);
+
+        btn_admin_control.setVisibility(View.VISIBLE);
+        btn_admin_stopcontrol.setVisibility(View.GONE);
+        btn_admin_opendoor.setVisibility(View.GONE);
     }
 
     public void clicklistener(){
@@ -108,7 +115,7 @@ public class AdminActivity extends AppCompatActivity {
                         new Thread(new Runnable() {
                             @Override
                             public void run() {
-
+                                stopcontrol();
                             }
                         }).start();
                     }
@@ -139,13 +146,39 @@ public class AdminActivity extends AppCompatActivity {
                             Toast.makeText(AdminActivity.this,"控制失败or已经控制！",Toast.LENGTH_SHORT).show();
                         }
                         else {
-                            Toast.makeText(AdminActivity.this,"控制失败or已经控制！",Toast.LENGTH_SHORT).show();
+                            Toast.makeText(AdminActivity.this,result,Toast.LENGTH_SHORT).show();
+                            edit_admin_boxselect.setEnabled(false);
+                            btn_admin_control.setVisibility(View.GONE);
+                            btn_admin_stopcontrol.setVisibility(View.VISIBLE);
+                            btn_admin_opendoor.setVisibility(View.VISIBLE);
+                            isControlling = true;
+                            control_order_number = result;
                         }
                     }catch (NullPointerException e){
                         e.printStackTrace();
                     }
+                    break;
                 }
-                break;
+                case ADMINSTOPCONTROL_JUDGE: {
+                    Bundle bundle = new Bundle();
+                    bundle = msg.getData();
+                    String result = bundle.getString("result");
+                    try {
+                        if (result.equals("finish_success")) {
+                            Toast.makeText(AdminActivity.this, "取消控制成功", Toast.LENGTH_SHORT).show();
+                            edit_admin_boxselect.setEnabled(true);
+                            btn_admin_control.setVisibility(View.VISIBLE);
+                            btn_admin_stopcontrol.setVisibility(View.GONE);
+                            btn_admin_opendoor.setVisibility(View.GONE);
+                            isControlling = false;
+                        } else {
+                            Toast.makeText(AdminActivity.this, "取消失败", Toast.LENGTH_SHORT).show();
+                        }
+                    }catch (NullPointerException e){
+                        e.printStackTrace();
+                    }
+                    break;
+                }
             }
         }
     };
@@ -153,22 +186,42 @@ public class AdminActivity extends AppCompatActivity {
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode==KeyEvent.KEYCODE_BACK){
-            AlertDialog.Builder builder=new AlertDialog.Builder(this);
-            builder.setTitle("提示：");
-            builder.setMessage("退出将停止控制，您确定退出？");
+            if (isControlling){
+                AlertDialog.Builder builder=new AlertDialog.Builder(this);
+                builder.setTitle("提示：");
+                builder.setMessage("退出将停止控制，您确定退出？");
 
-            //设置确定按钮
-            builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    finish();
-                }
-            });
-            //设置取消按钮
-            builder.setNegativeButton("取消",null);
-            //显示提示框
-            builder.show();
+                //设置确定按钮
+                builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        stopcontrol();
+                        finish();
+                    }
+                });
+                //设置取消按钮
+                builder.setNegativeButton("取消",null);
+                //显示提示框
+                builder.show();
+            }
+
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    public void stopcontrol(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //使用下面类里的函数，连接servlet，返回一个result，使用handler处理这个result
+                String result = HttpOrder.FinishOrder(control_order_number);
+                Bundle bundle = new Bundle();
+                bundle.putString("result",result);
+                Message message = new Message();
+                message.setData(bundle);
+                message.what = ADMINSTOPCONTROL_JUDGE;
+                handler.sendMessage(message);
+            }
+        }).start();
     }
 }
